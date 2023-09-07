188e0adc5d29b8611051d5afa5f54ae0c675c04f
ashisbe <ashis.behera@amdocs.com>
2023-09-07 10:55:47 +0530

    hello

--- Inventory_mang_root.java
+++ Inventory_mang_root.java
@@ -1,5 +1,6 @@
 public class Inventory_mang_root {
     public static void main(String[] args) {
         System.out.println("welcome team to the project");
+        System.out.println("Hello team");
     }
 }

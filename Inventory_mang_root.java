9ed168fc6a2872418b494539c90d3aaa121d4de1
Ayushiiism <ayushtripathy08@gmail.com>
2023-09-07 11:09:28 +0530

    I am Ayush

--- Inventory_mang_root.java
+++ Inventory_mang_root.java
@@ -2,5 +2,6 @@ public class Inventory_mang_root {
     public static void main(String[] args) {
         System.out.println("welcome team to the project");
         System.out.println("Hello team");
+        System.out.println("I am Ayush");
     }
 }

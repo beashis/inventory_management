1056d61182abf249a8da1ec5911dabbdf7cf2b5f
manirai <manirai@amdocs.com>
2023-09-09 11:26:37 +0530

    function_definition_for_admin

--- admin/admin_main.java
+++ admin/admin_main.java
@@ -3,14 +3,14 @@ package admin;
 public class admin_main {
     public void createProduct(int id, String name, int count)
     {
-
+        String query="insert into inventory(id,name,count) values ("+id+","+name+","+count+")";
     }
     public void  addProduct(int id,int amount)
     {
-
+        String query = "update inventory set count="+amount+"where id ="+id;
     }
     public void deleteProduct(int id,String name)
     {
-
+        String query ="delete from inventory where id="+id;
     }
 }
